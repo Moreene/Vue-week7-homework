6fea049a5374ba913d786bd75fbf422de8f8bacf
Moreene <moreenewu@gmail.com>
2024-02-25 09:20:56 +0800

    refactor：優化資料綁定/更新方式

--- src/components/OrderEditComponent.vue
+++ src/components/OrderEditComponent.vue
@@ -5,40 +5,40 @@
             <div class="modal-content border-0">
                 <div class="modal-header bg-info text-white">
                     <h5 id="editOrderModalLabel" class="modal-title">
-                        <span>{{ tempOrder.id }}</span>
+                        <span>{{ order.id }}</span>
                     </h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <h2 class="h5 fw-bold">訂單編號：{{ tempOrder.id }}</h2>
-                    <h2 class="h5 fw-bold">訂單日期：{{ new Date(tempOrder.create_at * 1000).toLocaleDateString() }}</h2>
+                    <h2 class="h5 fw-bold">訂單編號：{{ order.id }}</h2>
+                    <h2 class="h5 fw-bold">訂單日期：{{ new Date(order.create_at * 1000).toLocaleDateString() }}</h2>
                     <div class="row py-4">
                         <div class="col-lg-6">
-                            <div class="row gy-3">
+                            <div class="row gy-3" v-if="order.user">
                                 <div class="col-md-6">
                                     <label for="people" class="form-label">訂購人</label>
-                                    <input id="people" type="text" class="form-control" v-model="user.name" disabled>
+                                    <input id="people" type="text" class="form-control" v-model="order.user.name" disabled>
                                 </div>
                                 <div class="col-md-6">
                                     <label for="phone" class="form-label">手機</label>
-                                    <input id="phone" type="tel" class="form-control" v-model="user.tel" disabled>
+                                    <input id="phone" type="tel" class="form-control" v-model="order.user.tel" disabled>
                                 </div>
                                 <div class="col-md-6">
                                     <label for="mail" class="form-label">信箱</label>
-                                    <input id="mail" type="email" class="form-control" v-model="user.email" disabled>
+                                    <input id="mail" type="email" class="form-control" v-model="order.user.email" disabled>
                                 </div>
                                 <div class="col-md-6">
                                     <label for="address" class="form-label">地址</label>
-                                    <input id="address" type="text" class="form-control" v-model="user.address">
+                                    <input id="address" type="text" class="form-control" v-model="order.user.address">
                                 </div>
                                 <div class="col-12">
                                     <label for="extarea">留言</label>
                                     <textarea class="form-control" id="extarea" style="height: 100px;"
-                                        v-model="tempOrder.message"></textarea>
+                                        v-model="order.message"></textarea>
                                 </div>
                                 <div class="col-12">
                                     <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" v-model="tempOrder.is_paid" id="check">
+                                        <input class="form-check-input" type="checkbox" v-model="order.is_paid" id="check">
                                         <label class="form-check-label" for="check">
                                             已付款
                                         </label>
@@ -47,17 +47,17 @@
                             </div>
                         </div>
                         <div class="col-lg-6">
-                            <div v-if="tempOrder && tempOrder.products">
-                                訂購內容 ({{ Object.keys(tempOrder.products).length }})
+                            <div v-if="order && order.products">
+                                訂購內容 ({{ Object.keys(order.products).length }})
                             </div>
                             <hr class="my-2">
                             <ol class="ps-3 py-2">
-                                <li v-for="item in tempOrder.products" :key="item.id">
+                                <li v-for="item in order.products" :key="item.id">
                                     {{ item.product.title }} x {{ item.qty }}
                                 </li>
                             </ol>
                             <hr class="my-2">
-                            <p>總金額：{{ tempOrder.total }}</p>
+                            <p>總金額：{{ order.total }}</p>
                         </div>
                     </div>
                     <div class="modal-footer">
@@ -90,13 +90,13 @@ export default {
     props: ["tempOrder"],
     data() {
         return {
-            user: {},
+            order: {},
             editOrderModal: null,
         };
     },
     methods: {
         updateOrder() {
-            axios.put(`${VITE_URL}/api/${VITE_PATH}/admin/order/${this.tempOrder.id}`, { "data": this.tempOrder })
+            axios.put(`${VITE_URL}/api/${VITE_PATH}/admin/order/${this.tempOrder.id}`, { "data": this.order })
                 .then(res => {
                     this.$swal({
                         icon: "success",
@@ -117,7 +117,7 @@ export default {
     },
     watch: {
         tempOrder() {
-            this.user = this.tempOrder.user;
+            this.order = this.tempOrder;
         },
     },
     mounted() {
